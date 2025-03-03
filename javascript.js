048a7d6d790aada114e0233280377a5ed500d024
Chelsea Cruz <125297600+chelsea-here@users.noreply.github.com>
2025-03-03 17:56:12 -0600

    updated styling for each page. optimized for ipad and pc screen.  still need to work on mobile styling

--- javascript.js
+++ javascript.js
@@ -1,14 +1,17 @@
-// Get the container element
-var navLinkContainer = document.getElementById("navDiv");
+console.log("hello world");
 
-// Get all buttons with class="btn" inside the container
-var links = navLinkContainer.getElementsByClassName("navLink");
+//never got this to properly work. manual solution worked since navigation is redundant in this examole
+// // Get the container element
+// var navLinkContainer = document.getElementById("navDiv");
 
-// Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("activePage");
-    current[0].className = current[0].className.replace(" activePage", "");
-    this.className += " activePage";
-  });
-}
+// // Get all buttons with class="btn" inside the container
+// var btns = navLinkContainer.getElementsByClassName("navLink");
+
+// // Loop through the buttons and add the activePage class to the current/clicked button
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function () {
+//     var current = document.getElementsByClassName("activePage");
+//     current[0].className = current[0].className.replace(" activePage", "");
+//     this.className += " activePage";
+//   });
+// }
